2a45fbf57af06ca53f4d0e70fd331d33031556f7
Ananya <afmtrendaid@gmail.com>
2023-05-04 16:10:47 -0700

    progress bar and major styling

--- fetena/src/app/quizarea/quizarea.component.ts
+++ fetena/src/app/quizarea/quizarea.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { QuizapiService } from '../quizapi.service';
 
 @Component({
   selector: 'app-quizarea',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./quizarea.component.css']
 })
 export class QuizareaComponent {
+  public questions: any;
+  public questionsNumber:any;
+  public doneQuestions:any = 0;
+  public pro:any = "0%";
+  public progress:any = 0;
+  public visited = new Set();
+
+  constructor(private quizService:QuizapiService){
+    this.questions = this.quizService.questions;
+    this.questionsNumber =  this.questions.length;
+  }
+
+  incrementDone(i:number){
+
+     let found :boolean = false;
+    this.visited.forEach (function(value) {
+      if(value == i){
+        found = true;
+        
+      }
+    })
+    if(!found){
+      this.doneQuestions++;
+      this.progress = (this.doneQuestions/this.questionsNumber);
+      this.pro= String(this.progress*100) + "%";
+      this.visited.add(i);
+    }
+
+  }
+  
 
 }
