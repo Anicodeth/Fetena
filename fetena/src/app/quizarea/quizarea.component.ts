b8f802839a38126d448591ebbae2db8979db8fd9
Ananya <afmtrendaid@gmail.com>
2023-06-04 08:39:27 -0700

    Functionality Completed

--- fetena/src/app/quizarea/quizarea.component.ts
+++ fetena/src/app/quizarea/quizarea.component.ts
@@ -13,10 +13,10 @@ export class QuizareaComponent implements OnInit {
   public doneQuestions:any = 0;
   public answersGiven:any;
   public answersKey:any;
-  public pro:any = "0%";
   public progress:any = 0;
   public visited = new Set();
   public temp:any;
+  public enableButton:boolean = true;
 
 
   constructor(private quizService:QuizapiService,
@@ -25,19 +25,24 @@ export class QuizareaComponent implements OnInit {
   }
 
 
-incrementDone(i:number){
-    let found :boolean = false;
-    this.visited.forEach (function(value) {
-      if(value == i){
-        found = true;
-      }
-    });
+incrementDone(i:number, answer: any){
 
+    let found :boolean = false;
+
+    this.visited.forEach (function(value) {
+      if(value == i)  found = true;       });
+
+    this.answersGiven[i] = answer;
+    let count = 0;
+
+    this.answersGiven.forEach((value:any)=>{
+    if(value != null)  count ++; });
+
+    if (count == this.answersGiven.length) this.enableButton = false;
 
     if(!found){
       this.doneQuestions++;
-      this.progress = (this.doneQuestions/this.questionsNumber);
-      this.pro= String(this.progress*100) + "%";
+      this.progress = String((this.doneQuestions/this.questionsNumber)*100) + "%";
       this.visited.add(i);
     }
 
@@ -46,24 +51,41 @@ incrementDone(i:number){
   }
 
 
+  correct(){
+    let res = 0;
+    for(let i = 0; i < this.questionsNumber; i++){
+      if (this.answersGiven[i] == this.answersKey[i]) res ++;
+    }
+    return res
+  }
+
+  results(){
+    let result = this.correct();
+    let outOff = this.questionsNumber ;
+    this.router.navigate(['/result'], { queryParams: { result , outOff } });
+  }
+
+
+
   ngOnInit(): void {
     this.temp = sessionStorage.getItem('quizQuestions');
     if (this.temp){
+
       this.questions = JSON.parse(this.temp);
       this.questionsNumber =  this.questions.length;
       this.answersGiven  = new Array(this.questionsNumber);
       this.answersKey  = new Array(this.questionsNumber);
 
-      this.questions.forEach((value:any)=>{
-  
-      });
-      console.log(this.questions);
-    }else{
-      this.router.navigate(['quiz']);
-    }
+      for(let i = 0; i < this.questionsNumber; i++){
+       let j = 1;
+       for (const value in  (this.questions[i].correct_answers)){
+          if ((this.questions[i].correct_answers)[value] == 'true'){
+            this.answersKey[i] = String.fromCharCode(96 + j);}
+          j++;
+        };}
+
+    }else this.router.navigate(['quiz']);
 
   }
-  
-  
 
 }
--- fetena/src/app/quizresult/quizresult.component.ts
+++ fetena/src/app/quizresult/quizresult.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute , Router } from '@angular/router';
 
 @Component({
   selector: 'app-quizresult',
   templateUrl: './quizresult.component.html',
   styleUrls: ['./quizresult.component.css']
 })
-export class QuizresultComponent {
+export class QuizresultComponent implements OnInit{
 
+  public result : any;
+  public outOff : any;
+
+  constructor(private route: ActivatedRoute,
+    private router:Router
+    ) { }
+
+  redirectBack(){
+    this.router.navigate(['/']);
+  }
+  
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.result = params['result'];
+      this.outOff = params['outOff'];
+    });
+  }
+  
 }
